e6a7a2e882875e14aa6ddd98957147995d954cdf
Vlad Gorbich <vladgorbich@gmail.com>
2017-04-04 18:53:17 -0500

    add database mock code

--- src/com/emp/persistence/DatabaseConnectionManager.java
+++ src/com/emp/persistence/DatabaseConnectionManager.java
@@ -1,20 +1,41 @@
 package com.emp.persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnectionManager {
 
-	/*public DatabaseConnection getManagerInstance() {
-				
-	}*/
-	
-	public void connect() {
-		System.out.println("connecting");
+	private Connection conn;
+
+	private static DatabaseConnectionManager connectionInstance = new DatabaseConnectionManager();
+
+	private DatabaseConnectionManager() {
+
 	}
-	
-	public void disconnect() {
-		System.out.println("disconnecting");
+
+	public static DatabaseConnectionManager getManagerInstance() {
+		return connectionInstance;
 	}
-	
-	
-	
+
+	/**
+	 * contains connection details
+	 * 
+	 * @throws SQLException
+	 */
+	public void connect() throws SQLException {
+		// processing specific to database connection details...
+		conn = DriverManager.getConnection("Some/Database/URL");
+		System.out.println("Established Database Connection...");
+	}
+
+	public Connection getConnectionObject() {
+		return conn;
+	}
+
+	public void disconnect() throws SQLException {
+		conn.close();
+		System.out.println("Disconnected from Database...");
+	}
 	
 }
